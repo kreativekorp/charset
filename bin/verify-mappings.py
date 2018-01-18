ef3f8e5682ff580e253006978a66bef33e7a5f2e
Rebecca G. Bettencourt <beckiejo@gmail.com>
2018-01-17 23:38:45 -0800

    CJK encodings

--- bin/verify-mappings.py
+++ bin/verify-mappings.py
@@ -11,9 +11,11 @@ from acquisitionlib import acquire
 from parselib import atline_matcher, cd, charset_path, expand, ls, split_mapline
 
 __verify_matcher = atline_matcher('verify-against')
+__jis_x_0208_matcher = re.compile('^\\s*([0][Xx][0-9A-Fa-f]+)\\s+([0][Xx][0-9A-Fa-f]+)\\s+([0][Xx][0-9A-Fa-f]+)')
 __adobe_line_matcher = re.compile('^\\s*([0-9A-Fa-f]+)\\s+([0-9A-Fa-f]+)')
 __delimited_byte_matcher = re.compile('^\\s*([0][Xx][0-9A-Fa-f]{2}([+][0][Xx][0-9A-Fa-f]{2})+)\\s+[0][Xx]')
 __delimited_byte_sub = re.compile('[+][0][Xx]')
+__odd_hex_digits_sub = re.compile('^(\\s*[0][Xx])([0-9A-Fa-f]([0-9A-Fa-f]{2})*\\s+)')
 __apple_lr_sub = re.compile('(\\s+)<LR>(([+][0][Xx][0-9A-Fa-f]+)+)')
 __apple_rl_sub = re.compile('(\\s+)<RL>(([+][0][Xx][0-9A-Fa-f]+)+)')
 
@@ -35,6 +37,11 @@ def verify(path):
 		expmap = {}
 		with open(acquire(url), 'r') as f:
 			for line in f:
+				# Hacks for reference encodings: JIS X 0208 has three columns.
+				if url.endswith('/MAPPINGS/OBSOLETE/EASTASIA/JIS/JIS0208.TXT'):
+					m = __jis_x_0208_matcher.match(line)
+					if m is not None:
+						line = line[:m.start(1)] + m.group(2) + '\t' + m.group(3) + line[m.end(3):]
 				# Hacks for reference encodings: Adobe reverses bytes and chars.
 				if '/MAPPINGS/VENDORS/ADOBE/' in url:
 					m = __adobe_line_matcher.match(line)
@@ -49,6 +56,8 @@ def verify(path):
 				m = __delimited_byte_matcher.match(line)
 				if m is not None:
 					line = line[:m.start(1)] + __delimited_byte_sub.sub('', m.group(1)) + line[m.end(1):]
+				# Hacks for reference encodings: Odd number of hex digits in bytes.
+				line = __odd_hex_digits_sub.sub('\\g<1>0\\g<2>', line)
 				# Hacks for reference encodings: Apple's <LR> and <RL> markup.
 				line = __apple_lr_sub.sub('\\g<1>0x202D\\g<2>+0x202C', line)
 				line = __apple_rl_sub.sub('\\g<1>0x202E\\g<2>+0x202C', line)
@@ -56,20 +65,21 @@ def verify(path):
 				b, c, ba, ca = split_mapline(line)
 				if b is not None and c is not None:
 					expmap[tuple(b)] = tuple(c)
-		# Hacks for reference encodings: No ASCII characters.
-		if all((x,) not in expmap for x in range(0, 128)):
-			for x in range(0, 128):
-				expmap[(x,)] = (x,)
-		# Hacks for reference encodings: No C0 control characters.
-		if all((x,) not in expmap for x in range(0, 32) + [127]):
-			for x in range(0, 32) + [127]:
-				expmap[(x,)] = (x,)
-		# Hacks for reference encodings: No C1 control characters.
-		if '/MAPPINGS/VENDORS/ADOBE/' in url:
-			if any((x,) in expmap for x in range(160, 256)):
-				if all((x,) not in expmap for x in range(128, 160)):
-					for x in range(128, 160):
-						expmap[(x,)] = (x,)
+		if not url.endswith('/MAPPINGS/OBSOLETE/EASTASIA/OTHER/CNS11643.TXT'):
+			# Hacks for reference encodings: No ASCII characters.
+			if all((x,) not in expmap and (x,x) not in expmap for x in range(0, 128)):
+				for x in range(0, 128):
+					expmap[(x,)] = (x,)
+			# Hacks for reference encodings: No C0 control characters.
+			if all((x,) not in expmap and (x,x) not in expmap for x in range(0, 32) + [127]):
+				for x in range(0, 32) + [127]:
+					expmap[(x,)] = (x,)
+			# Hacks for reference encodings: No C1 control characters.
+			if '/MAPPINGS/VENDORS/ADOBE/' in url or '/MAPPINGS/OBSOLETE/EASTASIA/' in url:
+				if any((x,) in expmap for x in range(160, 256)) or url.endswith('/BIG5.TXT'):
+					if all((x,) not in expmap and (x,x) not in expmap for x in range(128, 160)):
+						for x in range(128, 160):
+							expmap[(x,)] = (x,)
 		# Hacks for reference encodings: Undefined characters mapped to U+FFFD.
 		for k, v in expmap.items():
 			if v == (0xFFFD,):
