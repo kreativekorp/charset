d9cd0b77f181e5fa4221f88ba940459ea8a9b554
Rebecca G. Bettencourt <beckiejo@gmail.com>
2019-05-26 13:28:59 -0700

    Update Vexillo PUA

--- acquisition/fonts/kreative-vexillo.py
+++ acquisition/fonts/kreative-vexillo.py
@@ -7,7 +7,7 @@ sys.path.append(os.path.normpath(os.path.join(os.path.dirname(os.path.realpath(_
 from acquisitionlib import acquire
 
 def list_fonts():
-	for code in ['GA', 'GC', 'GD', 'GF', 'GK', 'MA', 'MC', 'MD', 'MF', 'MK', 'VB', 'VF', 'VN']:
+	for code in ['GA', 'GB', 'GC', 'GD', 'GE', 'GF', 'GH', 'GK', 'GV', 'GW', 'MA', 'MB', 'MC', 'MD', 'ME', 'MF', 'MH', 'MK', 'MV', 'MW', 'VB', 'VF', 'VN']:
 		name = 'Kreative Vexillo %s' % code
 		url = 'https://github.com/kreativekorp/vexillo/raw/master/fonts/Vexillo/Vexillo%s.ttf.sbix.ttf' % code
 		yield (name, acquire(url, 'local'), 'https://github.com/kreativekorp/vexillo')
