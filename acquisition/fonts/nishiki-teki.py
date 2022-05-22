65d4248c857dd9c0a45c83de8155f4e2ba5a5eb5
Rebecca G. Bettencourt <beckiejo@gmail.com>
2022-05-22 14:24:06 -0700

    Update acq/nishiki-teki

--- acquisition/fonts/nishiki-teki.py
+++ acquisition/fonts/nishiki-teki.py
@@ -8,4 +8,7 @@ sys.path.append(os.path.normpath(os.path.join(os.path.dirname(os.path.realpath(_
 from acquisitionlib import acquire, cache_path
 
 def list_fonts():
-	yield ('Nishiki-teki', acquire('http://umihotaru.fool.jp/nishiki-teki.ttf', 'local'), 'https://umihotaru.work/')
+	with zipfile.ZipFile(acquire('https://umihotaru.work/nishiki-teki.zip', 'local'), 'r') as zip:
+		for info in zip.infolist():
+			if info.filename.endswith('.ttf'):
+				yield ('Nishiki-teki', zip.extract(info, cache_path()), 'https://umihotaru.work/')
